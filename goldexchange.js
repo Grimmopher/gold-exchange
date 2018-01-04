0bd904b729345c7e0f431bcdef667d296944249d
Christopher Grimm <chris.grimm@gmail.com>
2018-01-03 21:36:13 -0500

    Added coin definitions and default constructor values

--- goldexchange.js
+++ goldexchange.js
@@ -1,63 +1,75 @@
 const input = process.argv.slice(2, process.argv.length).join(' ');
 
-// Exchanges original 5e gold values to silver standard
-// Rate should be a string, ex '50g:1g' or '1p:3e'
+// Exchanges original 5e gold values to new standard
+// Rate should be a number or a string, ex '50:1' or 1/50
+// oldCoins and newCoins are arrays of objects contining
+// the sign and base value of each coin ex:
+// 1p = 1000c and 1g = 100c, ie: [ {sign: 'p', value: 1000}, {sign: 'g', value: 100} ]
+// Rate defaults to 10:1 to convert to a silver standard, and oldCoins and newCoins
+// default to 5e coin types, ie: p, g, e, s, c
 class Exchanger {
-    constructor({rate} = {}) {
-        let r = rate.split(':').map( v => this.convertToCopper(v) );
-        this.rate = r[1]/r[0];
+    constructor({rate, oldCoins, newCoins} = {}) {
+        this.convertToCopper = this.convertToBase.bind(this);
+        this.convertToCoins = this.convertToCoins.bind(this);
+        this.exchange = this.exchange.bind(this);
+
+        let defaultRate = 1/10;
+        let defaultCoins = [
+            {sign: 'p', value: 1000},
+            {sign: 'g', value: 100},
+            {sign: 'e', value: 50},
+            {sign: 's', value: 10},
+            {sign: 'c', value: 1}
+        ]
+        this.newCoins = newCoins == null ? defaultCoins : newCoins.sort( (a, b) => b.value - a.value );
+        this.oldCoins = oldCoins == null ? defaultCoins : oldCoins.sort( (a, b) => b.value - a.value );
+        if (rate == null) {
+            this.rate = defaultRate;
+        } else if (isNaN(rate)){
+            let r = rate.split(':').map( v => parseInt(v) );
+            this.rate = r[1]/r[0];
+        } else {
+            this.rate = rate;
+        }
     };
 
-    convertToCopper(value) {
+    convertToBase(value) {
         let vals = value.toLowerCase().split(' ');
         return vals.reduce( (total, val) => {
-            if (val.includes('c')) {
-                return total += parseInt(val);
-            };
-            if (val.includes('s')) {
-                return total += parseInt(val) * 10;
-            };
-            if (val.includes('e')) {
-                return total += parseInt(val) * 50;
-            };
-            if (val.includes('g')) {
-                return total += parseInt(val) * 100;
-            };
-            if (val.includes('p')) {
-                return total += parseInt(val) * 1000;
-            };
+            this.oldCoins.forEach( (coin) => {
+                if (val.includes(coin.sign)) {
+                    return total += parseInt(val) * coin.value;
+                }
+            });
+            return total;
         }, 0 );
-    };
+    }
 
     convertToCoins(value) {
-        let coins = '';
-        if (value/1000 >= 1) {
-            coins += Math.floor(value/1000) + 'p ';
-            value = value%1000;
-        }
-        if (value/100 >= 1) {
-            coins += Math.floor(value/100) + 'g ';
-            value = value%100;
-        }
-        if (value/50 >= 1) {
-            coins += Math.floor(value/50) + 'e ';
-            value = value%50;
-        }
-        if (value/10 >= 1) {
-            coins += Math.floor(value/10) + 's ';
-            value = value%10;
-        }
-        coins += value + 'c';
-        return coins
+        return this.newCoins.reduce( (convertedValue, coin) => {
+            if (value/coin.value >= 1) {
+                convertedValue += Math.floor(value/coin.value).toString() + `${coin.sign} `;
+                value = value%coin.value;
+            }
+            return convertedValue
+        }, '');
     }
 
     exchange(orig) {
-        let origCopper = this.convertToCopper(orig);
-        let newCopper = Math.round(origCopper * this.rate);
-        if (newCopper <= 0) newCopper = 1;
-        return this.convertToCoins(newCopper);
+        let origBase = this.convertToBase(orig);
+        let newBase = Math.round(origBase * this.rate);
+        if (newBase <= 0) newBase = 1;
+        return this.convertToCoins(newBase);
     };
 }
 
-let exchanger = new Exchanger({rate: '50g:1g'});
+let exchanger = new Exchanger({
+    rate: 1/50,
+    newCoins: [
+        {sign: 'g', value: 1000},
+        {sign: 'e', value: 100},
+        {sign: 's', value: 10},
+        {sign: 'c', value: 1}
+    ]
+});
 console.log(exchanger.exchange(input));
