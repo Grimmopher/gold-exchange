f4619c536c4bc39e1947bd661c034eb66a00e4b3
Christopher Grimm <chris.grimm@gmail.com>
2018-01-13 16:13:04 -0500

    building inline-source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/goldexchange.js',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'gold-exchange.js',
